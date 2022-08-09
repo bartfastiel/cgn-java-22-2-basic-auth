48444c6fc01f3f0128b89a0f693dfd2b24481538
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-08-09 21:45:38 +0200

    Show name of current user

--- backend/src/main/java/com/example/cgnjava222basicauth/SecurityConfig.java
+++ backend/src/main/java/com/example/cgnjava222basicauth/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/api/users/**").permitAll()
                 .antMatchers("/api/*").authenticated()
                 .and().httpBasic()
         ;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+    const [me, setMe] = useState<string>();
+
+    useEffect(() => {
+        axios.get("api/users/me")
+            .then(response => response.data)
+            .then(setMe)
+    }, [])
+
+    if (!me) {
+        return <>Loading...</>;
+    }
+
     return <>
-        hi
+        hi {me}
     </>
 }
 
